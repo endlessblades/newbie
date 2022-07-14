08a36797d1b3b1d8339eac2f0d3236b0e5869e62
endlessblades <moranurag10@gmail.com>
2022-07-14 19:23:35 +0530

    committry

---
+++ helooo.py
@@ -0,0 +1,178 @@
+# %%
+
+import os
+from turtle import done
+
+print ("hello ")
+print (24)
+
+a=23
+print (a)
+
+print('repl mode hhh')
+
+
+print(135415/2464)
+
+
+
+
+# %%
+a=56
+type(a)
+
+d=float(a)
+print(d)
+
+type(d)
+
+# %%
+name=input('who are you mothafoka?  ')
+print('welcome '+name)
+# %%
+
+
+print('next cell')
+# %%
+print('hello')
+# %%
+
+jimmi=input('my name is')
+print('hello',jimmi)
+# %%
+
+print(jimmi, "hello")
+
+eu=input('which eu floor?')
+us=(float(eu)+1)
+print(us)
+
+# %%
+
+# -now this is called comments f
+
+x=60
+if x < 11:
+    print ('hellaluyah')
+
+     
+if x>22:
+    print('dont fucking use else')
+
+
+     
+print ('booyah')
+
+
+
+
+# %%
+
+
+x=input(('choose the number bro'))
+x=float(x)
+if x==5:
+    print('its 5')
+if x <10:
+        print('less than 10')
+
+
+# %%
+
+print ('hello world')
+
+while true :
+    name=input('whats yr name bro?')
+    print ('its not you')
+    if name == 'Anurag' :
+        break 
+print (name +', you are the one !')
+
+# %%
+
+while True:
+    line = input('what line')
+    if line == 'done':
+        break
+    print (line)
+print('Done!')
+
+
+# %%
+
+while False:
+    linne = input ('what line bro?')
+    if linne == 'done':
+        break
+    print (linne)
+print ('Doen!')
+
+
+
+
+
+
+
+
+name = input('whats yr name bro ?')
+if name == 'Anurag':
+    print ('welcome back '+ name)
+else :
+    print (name+ ' you are not the one')
+
+# %%
+
+
+
+
+# this is an infinite loop - dont turn this on 
+
+n=1
+while True:
+    if n>0:
+        n=n+1
+    pnt(n)
+
+
+# %%
+
+import os
+
+print ('hi m8')
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# %%
+
+
+#this is for learning purposes 
